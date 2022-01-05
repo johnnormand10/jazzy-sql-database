97a138ea2f8573066d13d0e5206468bb6488e4ea
Johnathan Normand <jnormand10@gmail.com>
2022-01-04 18:49:13 -0600

    Added routers for both tables in the database

--- server/routes/artistList.router.js
+++ server/routes/artistList.router.js
@@ -1,16 +1,16 @@
 const pg = require('pg'); 
 const express = require('express');
-const router = express.Router();
+const artistRouter = express.Router();
 
 const pool = new pg.Pool({
-    database: 'artistList',
+    database: 'jazzy_sql',
     host: 'localhost',
     port: 5432
 });
 
-router.get('/', (req, res) => {
+artistRouter.get('/', (req, res) => {
 
-    const queryText = 'SELECT * FROM songs';
+    const queryText = 'SELECT * FROM artistList';
 
     pool.query(queryText)
         .then((dbRes) =>{
@@ -19,9 +19,36 @@ router.get('/', (req, res) => {
             
         })
         .catch((err) => {
-            console.log('GET /songs failed', err);
+            console.log('GET /artist failed', err);
             
             //Tell the client that it failed
             res.sendStatus(500);
         });
 });
+
+artistRouter.post('/', (req, res) =>{
+    console.log('req.body is ', req.body);
+
+    let queryText = `
+        INSERT INTO "artistList"
+            ("name", "birthdate")
+        VALUES
+            ($1, $2)
+    `;
+    
+    let queryParams = [
+        req.body.name,
+        req.body.birthdate
+    ]
+
+    pool.query(queryText, queryParams)
+        .then((dbRes) => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log('POST failed', err);
+            res.sendStatus(500);
+        })
+})
+
+module.exports = artistRouter;
